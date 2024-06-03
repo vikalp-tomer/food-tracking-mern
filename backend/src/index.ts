ed508a234af613b3a625f4a59cf65b85aefc9fdd
vikalp-tomer <vikalptomar15@gmail.com>
2024-06-03 18:29:14 +0530

    created basic create user in backend (routes, models and controllers)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import cors from 'cors'
 import 'dotenv/config';
 import mongoose from 'mongoose';
+import myUserRoute from './routes/MyUserRoute'
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => { console.log("Database connected") }).catch(err => console.log(err))
 
@@ -9,9 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/test', (req: Request, res: Response) => {
-    res.json({ message: "hello" })
-})
+app.use('/api/my/user', myUserRoute);
 
 app.listen(7000, () => {
     console.log(`Server started on localhost:7000`)
